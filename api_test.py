ef1cb2cae528a9c7eceb0e42ddf7d0656c8ea72e
Maryam Shahid <maryam.shahid@arbisoft.com>
2023-07-05 14:06:32 +0500

    Add exisiting files to Git

---
+++ api_test.py
@@ -0,0 +1,40 @@
+import requests
+
+#GET method
+api_url="https://jsonplaceholder.typicode.com/todos"
+response=requests.get(api_url)
+todo_list=response.json()
+incomplete=[]
+
+for todo in todo_list:
+    if todo['completed'] == False:
+        incomplete.append(todo)
+
+print(incomplete)
+
+#POST method
+new_todo={"userId":10, "id":2, "title":"things to do", "completed":False}
+response=requests.post(api_url,json=new_todo)
+response.json()
+print(response.json())
+
+#PUT method
+api_url="https://jsonplaceholder.typicode.com/todos/4"
+response=requests.get(api_url)
+response.json()
+
+updated_todo={"userId": 1, "id":4,"title": "Pay Bills", "completed": False}
+response=requests.put(api_url, json=updated_todo)
+response.json()
+print(response.json())
+
+# PATCH method
+todo={"completed":True}
+response=requests.patch(api_url,json=todo)
+response.json()
+print(response.json())
+
+ #DELETE method
+response=requests.delete(api_url)
+response.json()
+print(response.status_code)
