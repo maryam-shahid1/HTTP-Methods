1b5a2f195b3f98f46a95ae9df2e90dd92826748a
Maryam Shahid <maryam.shahid@arbisoft.com>
2023-07-06 15:34:03 +0500

    code refactoring

--- python_http.py
+++ python_http.py
@@ -3,6 +3,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 HOST="127.0.0.1"
 PORT=8000
 
+
 class WebRequestsHandler(BaseHTTPRequestHandler):
 
     def set_headers(self):
@@ -17,6 +18,7 @@ class WebRequestsHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         self.set_headers()
         self.wfile.write(bytes("<html><body><h1>POST method</h1></body></html>","utf-8"))
+
 
 server=HTTPServer((HOST,PORT), WebRequestsHandler)
 print("Server Now Running...")
