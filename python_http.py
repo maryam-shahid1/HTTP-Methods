1ba1371823510b313b280f397e14ff23d53db33d
Maryam Shahid <maryam.shahid@arbisoft.com>
2023-07-06 15:15:06 +0500

    code refactoring

--- python_http.py
+++ python_http.py
@@ -1,13 +1,13 @@
-from http.server import HTTPServer,BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
 
-PORT=8000
 HOST="127.0.0.1"
+PORT=8000
 
 class WebRequestsHandler(BaseHTTPRequestHandler):
 
     def set_headers(self):
         self.send_response(200)
-        self.send_header("Content Type","text/html")
+        self.send_header("Content-Type","text/html")
         self.end_headers()
 
     def do_GET(self):
@@ -18,7 +18,6 @@ class WebRequestsHandler(BaseHTTPRequestHandler):
         self.set_headers()
         self.wfile.write(bytes("<html><body><h1>POST method</h1></body></html>","utf-8"))
 
-
 server=HTTPServer((HOST,PORT), WebRequestsHandler)
-print("server now running")
+print("Server Now Running...")
 server.serve_forever()
